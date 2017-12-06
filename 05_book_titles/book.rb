e96a34b762131f9518ac4ce2d89e100757c62f5d
ParamagicDev <konnor5456@gmail.com>
2017-12-06 15:50:17 -0500

    Finished all 6 parts of odin project debugging

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,42 @@
+
+
 class Book
-# write your code here
+    def title
+        @title
+    end
+
+    def title=(string)
+        array = string.split(" ")
+        #at every space the word is stored into the array
+        new_string = ""
+
+        lowercase_words = ["a", "an", "and",  "the", "this", "is" , "in", "of" ]
+        #list to crosscheck
+
+        #iterates through each word in the array
+        array.each do |word|
+           if (lowercase_words.include? (word.downcase) )
+                new_string += word.downcase + " "
+                #all lowercase_words are automatically downcased
+           else
+                new_string += word.capitalize + " "
+           end
+        end
+
+        
+        #catches if the lowercase_word is the first word
+        chars = new_string.chars
+        #creates the array
+        first_letter = chars[0]
+        #takes the first letter. capitalizes it, the joins it back to form the same string
+        first_letter.capitalize!
+
+        chars[0] = first_letter
+        new_string = chars.join("")
+
+
+        @title = new_string.strip
+        
+    end
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,32 @@
 class Timer
-  #write your code here
+
+    attr_accessor :seconds
+
+    def initialize
+      @seconds = 0
+    end
+
+    def time_string
+      hours = (@seconds / 3600) #finds intial hour value
+      minutes = (@seconds % 3600) / 60 #checks whats left after hours have been divided
+      seconds = (@seconds % 3600) % 60 #checks second left
+
+      # CHECKS FOR DOUBLE DIGITS
+      if (hours < 10)
+        hours = "0" + hours.to_s
+      end
+
+      if (minutes < 10)
+        minutes = "0" + minutes.to_s
+      end
+
+      if (seconds < 10)
+        seconds = "0" + seconds.to_s
+      end
+
+
+      time_string = [hours, minutes, seconds]
+      time_string.join(":")
+    end
+
 end
